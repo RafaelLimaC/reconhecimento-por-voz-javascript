0c5f46d955636a069f276ba6a8e8adf9c78cd6d9
RafaelLimaC <srafalimacosta@gmail.com>
2023-06-18 17:18:53 -0300

    Incluindo validações do número

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -14,11 +14,13 @@ recognition.addEventListener('result', onSpeak);
 function onSpeak(e) {
     chute = e.results[0][0].transcript;
     exibeChuteNaTela(chute);
+    verificaSeChuteValido(chute);
 };
 
 function exibeChuteNaTela(chute) {
     elementoChute.innerHTML = `
         <div>Você disse</div>
         <span class="box">${chute}<span>
-    `
+    `;
+    
 }
