e9a27f7b0797207ae08ac66934e56694990b8188
RafaelLimaC <srafalimacosta@gmail.com>
2023-06-19 17:45:30 -0300

    Atualizando casos de validação do número

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,12 +1,10 @@
 const elementoChute = document.getElementById('chute');
 
-
-
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 
-recognition.lang = 'pt-Br';
+recognition.lang = 'pt-BR';
 recognition.start();
 
 recognition.addEventListener('result', onSpeak);
@@ -22,5 +20,38 @@ function exibeChuteNaTela(chute) {
         <div>Você disse</div>
         <span class="box">${chute}<span>
     `;
-    
+
 }
+
+recognition.addEventListener('end', () => recognition.start())
+
+
+
+
+
+
+// solução para numeros menores que 10
+const numeros = {
+    'zero zero': 0,
+    '00': 0,
+    '01': 1,
+    'um': 1,
+    'dois': 2,
+    'três': 3,
+    'quatro': 4,
+    'cinco': 5,
+    'seis': 6,
+    'sete': 7,
+    'oito': 8,
+    'nove': 9,
+    'dez': 10
+ }
+ 
+ const corrigeNumeros = (palavra) => {
+    for(numero in numeros){
+       if(palavra === numero){
+          palavra = numeros[numero];   
+       }         
+    }
+    return palavra;
+ }
--- app/validacao.js
+++ app/validacao.js
@@ -1,12 +1,32 @@
 function verificaSeChuteValido(chute) {
-    const numero =+ chute;
+    const numero = +chute;
 
     if (seChuteInvalido(numero)) {
-        console.log("Valor inválido.")
+        elementoChute.innerHTML += '<div>Valor inválido.</div>';
+        return;
     }
 
     if (numeroMaiorQueValorMaximo(numero)) {
-        console.log(`Valor inválido. O número precisa entre: ${menorValor} e ${maiorValor} `)
+        elementoChute.innerHTML += `
+        <div>Valor inválido. Fale um número entre: 
+        ${menorValor} e ${maiorValor} </div>
+        `;
+        return;
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era: ${numeroSecreto}</h3>
+        `;
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `;
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `;
     }
 }
 
